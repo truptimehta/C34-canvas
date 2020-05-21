3860b4a97c0c80a3b6f1bcc71c656687b5768e79
truptimehta <63162871+truptimehta@users.noreply.github.com>
2020-05-21 13:44:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,76 @@
 var database;
-var ref;
+var ref,countRef;
 var drawing=[];
-var dbDrawing=[]
+var dbDrawing=[];
+var currentpath=[];
+var drawPath=[];
+isDrawing=false
+var count=0
+var dname;
 
 function setup() {
 
   //Connection to database
   database=firebase.database();
-  console.log(database)
+  
 
   //refer to the node of database
   ref= database.ref('drawing');
+ 
+
  createCanvas(800,400);
 
 
  //listen to change in database
  ref.on("value",drawSketch,showError)
+ 
   
 }
 
 function draw() {
   background(255,255,255); 
  
+  if(isDrawing)
+  
+  {
+    var point={
+      x:mouseX,
+      y:mouseY
+    }
+    currentpath.push(point)
+
+    //create different paths for every mousePressed
+    dname="drawing/path"+count
+    console.log(dname)
+
+   //update database in node drawing /path and set node d: to points
+    database.ref(dname).set({d:currentpath})
+   
+  }
  //Give color and weight for drawing
   stroke("red")
   strokeWeight(4)
   noFill()
+  
+ 
+if(dbDrawing!==undefined)
+{
+  for(var path in dbDrawing)
+  {
+    drawPath=dbDrawing[path].d
+    if(drawPath!==undefined)
+    {
+    beginShape()
+    for(var j in drawPath)
+    {
+      vertex(drawPath[j].x,drawPath[j].y)
+    }
+    endShape()
+  }
+  }
+}
 
-
-  //start drawing with connecting all points in db
-   beginShape()
-   
-   for (i=0;i<dbDrawing.length;i++)
-   {
-    
-      vertex(dbDrawing[i].x,dbDrawing[i].y)
-   }
-   endShape()
+  
 
   
   drawSprites();
@@ -45,30 +79,23 @@ function draw() {
 function mouseDragged()
 {
 
-//store points of location to draw sketch
-  point={
-    x:mouseX,
-    y:mouseY
-  }
-  //store point in local variable
-  drawing.push(point);
+  isDrawing=true
 
-  //update database in node drawing and set node d: to points
-  database.ref('drawing').set({
-    'd':drawing
-  })
 }
 
 function mouseReleased()
 {
-
+  isDrawing=false;
+  count=count+1
+  currentpath=[]
 }
+
 
 
 //Read data from database and store value of d in variable
 function drawSketch(data)
 {
-  dbDrawing=data.val().d;
+  dbDrawing=data.val();
  
 }
 
